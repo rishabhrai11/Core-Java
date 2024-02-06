b8e813418c9bcf9054dd6fd4b06b6f75fe1c0579
Rishabh Rai <108413835+rishabhrai11@users.noreply.github.com>
2024-02-06 11:47:31 +0530

    Add files via upload

---
+++ Pattern/Star6.java
@@ -0,0 +1,23 @@
+package Mypackage;
+
+import java.util.Scanner;
+
+public class Star6 {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if((i==0&&j<(n-1)/2&&j!=0)||(i==(n-1)/2&&j<=(n-1)/2)||(i!=0&&j==0)||(i!=0&&j==(n-1)/2))
+						System.out.print('*');
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
