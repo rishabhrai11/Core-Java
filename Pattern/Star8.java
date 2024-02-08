69f795d40ce6b6e5860d3d9c23b1e8321a08f7a8
Rishabh Rai <108413835+rishabhrai11@users.noreply.github.com>
2024-02-08 10:33:10 +0530

    Add files via upload

---
+++ Pattern/Star8.java
@@ -0,0 +1,56 @@
+package Mypackage;
+import java.util.Scanner;
+public class Star8 {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==j)
+						System.out.print('*');
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==n-1-j)
+						System.out.print('*');
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==n-1-j||i==j)
+						System.out.print('*');
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==n-1-j||i==j||j==0||j==n-1||i==0||i==n-1)
+						System.out.print('*');
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
