334cfb19cd2720bfc9afd34c1193dab606b68d08
Rishabh Rai <108413835+rishabhrai11@users.noreply.github.com>
2024-02-13 22:19:53 +0530

    Add files via upload

---
+++ MixJava/Aeroplane.java
@@ -0,0 +1,12 @@
+package Mypackage;
+
+class Aeroplane {
+	public void takeOff() 
+	{
+		System.out.println("Plane is takeing off");
+	}
+	public void fly() //overridden method
+	{
+		System.out.println("Plane is flying");
+	}
+}
