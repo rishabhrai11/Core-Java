483acea75402afa79b42e06b692db150d868b876
Rishabh Rai <108413835+rishabhrai11@users.noreply.github.com>
2024-03-04 21:21:00 +0530

    Add files via upload

---
+++ MixJava/AssignmentString.java
@@ -0,0 +1,20 @@
+package Mypackage;
+
+public class AssignmentString {
+	public static void main(String args []) {
+		String s="PW SKILLS";
+		String s1="";
+		String arr[]=s.split(" ");
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr[arr.length-1-i].length();j++){
+				if(i==0)
+					s1+=arr[arr.length-1-i].charAt((arr[arr.length-1-i].length())-1-j);
+				else
+					s1+=arr[arr.length-1-i].charAt(j);
+			}
+			
+		}
+		System.out.println("Revised String:"+s1);
+	}
+}
